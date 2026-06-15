a777b31660cdef55210ffa7f932e7ed1bd6ae9b0
Pra <155552124+bistpramod@users.noreply.github.com>
2026-06-15 20:31:34 +0545

    Delete Node.js directory

--- Node.js/Node.js/index.js
+++
@@ -1,6 +0,0 @@
-(function (exports, require, module, __filename, __dirname) {
-});  // its an IIFE function wrapper for every module in node.js and it is used for scoping and module management.
-
-
-console.log(__filename)
-console.log(__dirname)  // just removes the file name from the path and gives only the directory path.
